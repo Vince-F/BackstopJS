240cec5bcdcac11662c0921bf652f963b5c37112
Vince <vincent.francolin@outlook.com>
2021-04-18 18:14:44 +0200

    Add action script to engine scripts in puppeteer

--- capture/engine_scripts/puppet/onReady.js
+++ capture/engine_scripts/puppet/onReady.js
@@ -1,6 +1,7 @@
 module.exports = async (page, scenario, vp) => {
   console.log('SCENARIO > ' + scenario.label);
   await require('./clickAndHoverHelper')(page, scenario);
+  await require('./actions')(page, scenario);
 
   // add more ready handlers here...
 };
